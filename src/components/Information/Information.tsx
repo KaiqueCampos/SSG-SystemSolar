a42eba361b4a60c37637096c4ac788f42ff7d479
Kaique Campos <kaiquecamposferreira@gmail.com>
2021-06-03 14:43:37 -0300

    save

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -1,4 +1,5 @@
 import styles from './styles.module.scss';
+import Image from 'next/image';
 
 type InformationProps = {
     left: string;
@@ -7,9 +8,14 @@ type InformationProps = {
 
 export default function Information(props: InformationProps) {
     return (
-        <div className={styles.container} style={{left: props?.left, right: props?.right}}>
+        <div className={styles.container} style={{ left: props?.left, right: props?.right }}>
             <div>
-                <img src='/satellite.svg' />
+                <Image
+                    src='/satellite.svg'
+                    objectFit='cover'
+                    height={70}
+                    width={70}
+                />
                 <h3>Satellite</h3>
                 <p>
                     Lorem Ipsum is simply dummy text of the...
@@ -17,7 +23,12 @@ export default function Information(props: InformationProps) {
             </div>
 
             <div>
-                <img src='/moon.svg' />
+                <Image
+                    src='/moon.svg'
+                    height={70}
+                    width={70}
+                    objectFit='cover'
+                />
                 <h3>Moon's</h3>
                 <p>
                     Lorem Ipsum is simply dummy text of the...
@@ -25,7 +36,12 @@ export default function Information(props: InformationProps) {
             </div>
 
             <div>
-                <img src='/Temperature.svg' />
+                <Image
+                    src='/temperature.svg'
+                    height={70}
+                    width={70}
+                    objectFit='cover'
+                />
                 <h3>Temperature</h3>
                 <p>
                     Lorem Ipsum is simply dummy text of the...
