1f6db9d772879651b5f2f3dfc2b0bf6ebee144b5
Kaique Campos <kaiquecamposferreira@gmail.com>
2021-06-04 10:45:48 -0300

    SSG already

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss';
 import Image from 'next/image';
 
 export default function Menu() {
+
     return (
         <div className={styles.container}>
             <h1> The Solar System </h1>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,41 @@ import Menu from "../components/Menu/Menu";
 import SolarSystem from "../components/SolarSystem/SolarSystem";
 import styles from '../styles/app.module.scss';
 
-export default function Home() {
-  return (
+type Star = {
+  name: string;
+  diameter: string;
+  distanceFromSun: string;
+  image: string;
+  description: string;
+  satellites: string;
+  moons: string;
+  color: string;
+}
 
+type HomeProps = {
+  stars: Star[]
+}
+
+export default function Home({ stars }: HomeProps) {
+
+  stars.map(star => console.log(star.name))
+
+  return (
     <div className={styles.container}>
       <Menu />
       <SolarSystem />
       <Information left='none' right='2rem' />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/stars');
+  const data = await response.json()
+  return {
+    props: {
+      stars: data
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
 }
